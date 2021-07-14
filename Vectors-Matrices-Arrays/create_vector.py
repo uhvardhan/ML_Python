101639d65e811c0be64a5cc5f3ff0dc9b5210f60
Harshvardhan Uppaluru <iamharsh@PheonixintheAir.fios-router.home>
2021-07-14 11:03:40 -0400

    first commit

---
+++ Vectors-Matrices-Arrays/create_vector.py
@@ -0,0 +1,16 @@
+# Load library
+import numpy as np
+
+# Create a vector as a row
+vector_row = np.array([1, 2, 3])
+
+# Create a vector as a column
+vector_column = np.array([[1],
+                          [2],
+                          [3]])
+
+# Print statements
+print("One dimensional row:")
+print(vector_row)
+print("One dimensional column:")
+print(vector_column)
